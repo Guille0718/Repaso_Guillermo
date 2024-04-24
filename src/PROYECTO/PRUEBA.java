57e1f78609cb0d8ce6070075f390c31cf4edeaef
guill <guill@GUILLE>
2024-04-24 12:10:55 +0200

    Soy Guillermo

--- src/PROYECTO/PRUEBA.java
+++ src/PROYECTO/PRUEBA.java
@@ -5,6 +5,9 @@ public class PRUEBA {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola");
+			System.out.println("¿Que tal?");
+			System.out.println("¿Donde estas?");
+			System.out.println("Adios");
 	}
-
+	
 }
