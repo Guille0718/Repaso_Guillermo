2b000770a35422e4c2398b9c1be2718a496408c2
Erji <Erji@BlackPanda>
2024-05-08 12:15:38 +0200

    Ramificacion

--- src/PROYECTO/PRUEBA.java
+++ src/PROYECTO/PRUEBA.java
@@ -4,10 +4,8 @@ public class PRUEBA {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hola");
-			System.out.println("¿Que tal?");
-			System.out.println("¿Donde estas?");
-			System.out.println("Adios");
+		System.out.println("Me gustan las patatas");
+		
 	}
 	
 }
