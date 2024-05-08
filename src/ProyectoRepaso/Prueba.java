55f3bd94378b9ae5f882556677cfc569c1bc39c9
guill <guill@GUILLE>
2024-05-08 12:01:11 +0200

    Repaso Guillermo

---
+++ src/ProyectoRepaso/Prueba.java
@@ -0,0 +1,12 @@
+package ProyectoRepaso;
+
+public class Prueba {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hola, soy Guillermo");
+		System.out.println("¿Que tal?");
+		
+	}
+
+}
